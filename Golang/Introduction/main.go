d3d4f4e2d4f8ef47949ac26cafce2cdcb842ec3e
Omkar <opkulkarni0104@gmail.com>
2022-12-21 09:30:19 +0530

    Added comments for Hello-world program

--- Golang/Introduction/main.go
+++ Golang/Introduction/main.go
@@ -1,9 +1,12 @@
 // simple hello-world program
 
-package main
+package main 	// package name
 
-import "fmt"
+import "fmt"	// fmt is the packae which includes various functions for input and output
 
-func main(){
-	fmt.Println("Hello World")
-}
+// main is the entry point of the executable program
+// fmt "." = is the dot operator , so we can access other functions of fmt package
+
+func main(){	 
+	fmt.Println("Hello World") 		// print the string
+ }
