19748ad7f5177b5290700d6b599a7b872fdcdb0a
Omkar <opkulkarni0104@gmail.com>
2022-12-24 09:06:26 +0530

    arithmatic operators added

--- Golang/operators/main.go
+++ Golang/operators/main.go
@@ -6,16 +6,42 @@ func main(){
 	// operators
 	// comparison operators
 	// ==, !=, <, >, <=, >=
-    a := 45
-	b := 89
-	fmt.Println(a == b)
-	fmt.Println(a != b)
-	fmt.Println(a < b)
-	fmt.Println(a > b)
+    // a := 45
+	// b := 89
+	// fmt.Println(a == b)
+	// fmt.Println(a != b)
+	// fmt.Println(a < b)
+	// fmt.Println(a > b)
 
-	name := "Omkar"
-	name2 := "vedant"
+	// name := "Omkar"
+	// name2 := "vedant"
 
-	fmt.Println(name == name2)
-	
+	// fmt.Println(name == name2)
+
+	// Arithmatic operators
+	// +, -, *, /, %
+	x , y := 10, 34
+	fmt.Println(x + y)
+	fmt.Println(x - y)
+	fmt.Println(x * y)
+	fmt.Println(x / y)
+	fmt.Println(x % y)
+	x++
+	y--
+	fmt.Println(x)
+	fmt.Println(y)
+
+	str1 , str2 := "one", "two"
+	fmt.Println(str1 + str2)
+
+	// var a float64 = 5.9
+	// a++
+	// fmt.Print(a)
+
+	var a, b float64 = 24.4, 3.0
+        fmt.Println(a / b)
+        fmt.Println(int(a) % int(b))
+
+
+
 }
