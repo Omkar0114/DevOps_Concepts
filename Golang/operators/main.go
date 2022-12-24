ba710fa6afead02572c34dbef3bb084e3dd3a3e2
Omkar <opkulkarni0104@gmail.com>
2022-12-24 08:23:35 +0530

    Comparison operators added

---
+++ Golang/operators/main.go
@@ -0,0 +1,21 @@
+package main 
+
+import "fmt"
+
+func main(){
+	// operators
+	// comparison operators
+	// ==, !=, <, >, <=, >=
+    a := 45
+	b := 89
+	fmt.Println(a == b)
+	fmt.Println(a != b)
+	fmt.Println(a < b)
+	fmt.Println(a > b)
+
+	name := "Omkar"
+	name2 := "vedant"
+
+	fmt.Println(name == name2)
+	
+}
