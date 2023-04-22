bcf9a6bf877c8d4048ae842ebca13cf5d561f47d
Omkar <opkulkarni0104@gmail.com>
2023-04-22 11:51:38 +0530

    gorilla/mux package added

---
+++ Golang/Advanced-Go/go.mod
@@ -0,0 +1,5 @@
+module github.com/Omkar0114/DevOps_Concepts/Golang/Advanced-Go
+
+go 1.18
+
+require github.com/gorilla/mux v1.8.0
--- Golang/Advanced-Go/main.go
+++ Golang/Advanced-Go/main.go
@@ -5,10 +5,13 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
+	// "gorilla/mux"
 )
 
 type Product struct {
-	Id       int
+	Id       string
 	Name     string
 	Quantity int
 	Pirce    float64
@@ -22,9 +25,20 @@ func homepage(w http.ResponseWriter, r *http.Request) {
 }
 
 func handleAllRequests() {
-	http.HandleFunc("/home", homepage)
-	http.HandleFunc("/products", returAllProducts)
-	http.ListenAndServe("localhost:10000", nil)
+	myRouter := mux.NewRouter().StrictSlash(true)
+	myRouter.HandleFunc("/home", homepage)
+	myRouter.HandleFunc("/products", returAllProducts)
+	myRouter.HandleFunc("/product/{id}", getProduct)
+	http.ListenAndServe("localhost:10000", myRouter)
+}
+func getProduct(w http.ResponseWriter, r *http.Request){
+	vars := mux.Vars(r)
+	id := vars["id"]
+	for _, product := range Products {
+		if product.Id == id{
+			json.NewEncoder(w).Encode(product)
+		}
+   }
 }
 
 func returAllProducts(w http.ResponseWriter, r *http.Request) {
@@ -35,8 +49,8 @@ func returAllProducts(w http.ResponseWriter, r *http.Request) {
 func main() {
 	
 	Products = []Product{
-		{Id: 1, Name: "Pen", Quantity: 10, Pirce: 5.99},
-		{Id: 2, Name: "Pencil", Quantity: 5, Pirce: 2.99},
+		{Id: "1", Name: "Pen", Quantity: 10, Pirce: 5.99},
+		{Id: "2", Name: "Pencil", Quantity: 5, Pirce: 2.99},
 	}
 
 	handleAllRequests()
